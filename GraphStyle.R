74d2c386a9fb529d43a9269b50d514510953c818
bskwon0526 <bskwon0526@gmail.com>
2019-06-14 20:50:55 +0900

    commit

---
+++ GraphStyle.R
@@ -0,0 +1,30 @@
+
+library('ggplot2')
+dataframe <- read.csv("captcha.csv")
+
+str(dataframe)
+
+m1 <- lm(formula = dataframe$crec ~ dataframe$ctime * dataframe$itime)
+
+#reg : crec = 19018 + 0.1153*itme + 0.0306*ctime  
+
+summary(m1)
+print("------------------------------")
+fit1 <- lm(crec~itime*ctime,data=dataframe)
+summary(fit1)
+
+
+p <- ggplot(data=dataframe,aes(x=itime,y=crec,colour=ctime)) + geom_point() + geom_smooth(method="lm")
+
+
+p<-p+geom_abline(intercept=fit1$coef[1],slope=fit1$coef[2],colour="orange",linetype=2)
+p<-p+geom_abline(intercept=fit1$coef[1]+fit1$coef[3],slope=fit1$coef[2]+fit1$coef[4],
+                 colour="green",linetype=2)
+
+p
+
+model <-lm(crec ~ itime + ctime,data = dataframe)
+summary(model)
+#ctime = 0.17763 * ctime + 0.21005 * itime + 1.66375
+plot(model)
+
--- MultipleRegressionAnalysis.R
+++ MultipleRegressionAnalysis.R
@@ -1,3 +1,4 @@
+library('ggplot2')
 dataframe <- read.csv("captcha.csv")
 
 str(dataframe)
@@ -7,3 +8,24 @@ m1 <- lm(formula = dataframe$crec ~ dataframe$ctime + dataframe$itime)
 #reg : crec = 19018 + 0.1153*itme + 0.0306*ctime  
 
 summary(m1)
+
+plot(dataframe$crec~dataframe$ctime,xlab="value c time",ylab="value c recognition")
+abline(m2,col='pink')
+
+g <- ggplot(data=dataframe,aes(x=ctime,y=crec))
+g+geom_point(colour="grey60")+stat_smooth(method=lm,level=0.99)
+
+g2 <- ggplot(data=dataframe,aes(x=itime,y=crec))
+g2+geom_point(colour="grey60")+stat_smooth(method=lm,level=0.99,colour="Red")
+
+g3 <- ggplot(data=dataframe,aes(x=itime,y=ctime))
+g3+geom_point(colour="grey60")+stat_smooth(method=lm,level=0.99,colour="Orange")
+
+####
+
+fit <- lm(formula = dataframe$crec ~ dataframe$ctime + dataframe$itime)
+summary(fit)
+
+coef(fit)
+intercept =coef(fit)
+slope= coef(fit)
